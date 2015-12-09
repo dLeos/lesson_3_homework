939ac956960c9552a7512c3284879d45c4ec02cf
Дмитрий Обухов < >
2015-12-09 18:48:25 +0600

    Changed colculator.py, main.py

--- calculator.py
+++ calculator.py
@@ -3,24 +3,23 @@ class Calculator:
         self.base_numeral_system = 10
 
     def add(self, x, y):
-        """This function adds two numbers"""
         return x + y
 
     def subtract(self, x, y):
-        """This function subtracts two numbers"""
         return x - y
 
     def multiply(self, x, y):
-        """This function multiplies two numbers"""
         return x * y
 
     def divide(self, x, y):
-        """This function divides two numbers"""
         if y == 0:
             raise ZeroDivisionError
         return x / y
 
     def evaluate(self, expression):
-        """This function evaluate expression"""
-        return eval(expression)
+        try:
+            result = eval(expression)
+        except:
+            raise SyntaxError
+        return result
 
--- main.py
+++ main.py
@@ -2,40 +2,55 @@ from calculator import Calculator
 
 calc = Calculator()
 
+def read_number():
+    number = input("Enter first number: ")
+    try:
+        number = float(number)
+    except ValueError:
+        raise ValueError
+    return number
+
 while True:
     print("Menu:\n"
-          "1. Add\n""
+          "1. Add\n"
           "2. Substract\n"
-          "3. Multiply\n""
-          "4. Divide\n""
-          "5. Evaluate\n""
+          "3. Multiply\n"
+          "4. Divide\n"
+          "5. Evaluate\n"
           "0. Exit\n")
 
     action = input("What do you want? ")
 
-    if action == 0:
-        break;
+    if action == '0':
+        break
 
-    elif action == 5:
+    elif action == '5':
         expression = input("Enter the expression: ")
         try:
             result = calc.evaluate(expression)
         except SyntaxError:
-            result = "Error: incorrect expression."
+            print("Error: incorrect expression.\n")
+            continue
 
     else:
-        x = input("Enter first number: ")
-        y = input("Enter second number: ")
-        if action == 1:
+        try:
+            x = read_number()
+            y = read_number()
+        except ValueError:
+            print("Error: incorrect number")
+            continue
+
+        if action == '1':
             result = calc.add(x, y)
-        if action == 2:
+        if action == '2':
             result = calc.subtract(x, y)
-        if action == 3:
+        if action == '3':
             result = calc.multiply(x, y)
-        if action == 4:
+        if action == '4':
             try:
                 result = calc.divide(x, y)
             except ZeroDivisionError:
-                result = "Error: division by zero."
-    print("Result: ")
+                print("Error: incorrect number")
+                continue
+    print("Result: ", result)
 
