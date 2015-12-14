4516afe693104236eb4afb8669968878fff53055
Дмитрий Обухов < >
2015-12-14 14:24:56 +0600

    Исправления недочетов

--- calculator.py
+++ calculator.py
@@ -3,20 +3,41 @@ class Calculator:
         self.base_numeral_system = 10
 
     def add(self, x, y):
-        return x + y
+        """This function adds two numbers"""
+        try:
+            result = float(x + y)
+        except:
+            raise SyntaxError
+        return result
 
     def subtract(self, x, y):
-        return x - y
+        """This function subtracts two numbers"""
+        try:
+            result = float(x - y)
+        except:
+            raise SyntaxError
+        return result
 
     def multiply(self, x, y):
-        return x * y
+        """This function multiplies two numbers"""
+        try:
+            result = float(x * y)
+        except:
+            raise SyntaxError
+        return result
 
     def divide(self, x, y):
+        """This function divides two numbers"""
         if y == 0:
             raise ZeroDivisionError
-        return x / y
+        try:
+            result = float(x / y)
+        except:
+            raise SyntaxError
+        return result
 
     def evaluate(self, expression):
+        """This function evaluate expression"""
         try:
             result = eval(expression)
         except:
--- calculator_test.py
+++ calculator_test.py
@@ -1,31 +1,56 @@
+import unittest
 from unittest import TestCase
 from calculator import Calculator
-import unittest
+from genty import genty, genty_dataset
 
+@genty
 class CalculatorTest(TestCase):
     def setUp(self):
         self.calc = Calculator()
 
-    def test_add(self):
-        self.assertEqual(12, self.calc.add(5, 7))
+    @genty_dataset((5, 7, 12), (5.1, 7.1, 12.2), (-5.1, 7.1, 2.0))
+    def test_add_correct(self, x, y, result):
+        self.assertEqual(result, self.calc.add(x, y))
 
-    def test_subtract(self):
-        self.assertEqual(-2, self.calc.subtract(5, 7))
+    @genty_dataset(('ab', 7, SyntaxError), (5, 'cd', SyntaxError), ('ab', 'cd', SyntaxError))
+    def test_add_not_correct(self, x, y, result):
+        self.assertRaises(result, lambda: self.calc.add(x, y))
 
-    def test_multiply(self):
-        self.assertEqual(35, self.calc.multiply(5, 7))
+    @genty_dataset((5, 7, -2), (5.1, 7.1, -2.0), (-5.1, 7.9, -13))
+    def test_subtract_correct(self, x, y, result):
+        self.assertEqual(result, self.calc.subtract(x, y))
 
-    def test_divide(self):
-        self.assertEqual(float(5/7), self.calc.divide(5, 7))
+    @genty_dataset(('ab', 7, SyntaxError), (5, 'cd', SyntaxError), ('ab', 'cd', SyntaxError))
+    def test_subtract_not_correct(self, x, y, result):
+        self.assertRaises(result, lambda: self.calc.subtract(x, y))
 
-    def test_division_by_zero(self):
-        self.assertRaises(ZeroDivisionError, lambda: self.calc.divide(5, 0))
+    @genty_dataset((5, 7, 35), (5.1, 7.1, 36.21), (-5.1, 7.1, -36.21))
+    def test_multiply_correct(self, x, y, result):
+        self.assertAlmostEqual(result, self.calc.multiply(x, y))
 
-    def test_evaluate(self):
-        self.assertEqual(5, self.calc.evaluate("(5+7*5)/2**3"))
+    @genty_dataset(('ab', 7, SyntaxError), (5, 'cd', SyntaxError), ('ab', 'cd', SyntaxError))
+    def test_multiply_not_correct(self, x, y, result):
+        self.assertRaises(result, lambda: self.calc.multiply(x, y))
 
-    def test_evaluate_error(self):
-        self.assertRaises(SyntaxError, lambda: self.calc.evaluate("sin(PI)"))
+    @genty_dataset((5, 7, float(5/7)), (5.1, 7.1, float(5.1/7.1)), (-5.1, 7.1, -float(5.1/7.1)))
+    def test_divide_correct(self, x, y, result):
+        self.assertAlmostEqual(result, self.calc.divide(x, y))
+
+    @genty_dataset(('ab', 7, SyntaxError), (5, 'cd', SyntaxError), ('ab', 'cd', SyntaxError))
+    def test_divide_not_correct(self, x, y, result):
+        self.assertRaises(result, lambda: self.calc.divide(x, y))
+
+    @genty_dataset((5, 0, ZeroDivisionError), (7.1, 0, ZeroDivisionError))
+    def test_division_by_zero(self, x, y, result):
+        self.assertRaises(result, lambda: self.calc.divide(x, y))
+
+    @genty_dataset(("(5+7*5)/2**3", 5), ("5&3", 1), ("5^3", 6), ("5|3", 7))
+    def test_evaluate_correct(self, expression, result):
+        self.assertEqual(result,  self.calc.evaluate(expression))
+
+    @genty_dataset(("sin(PI)", SyntaxError), (")2+3(", SyntaxError))
+    def test_evaluate_not_correct(self, expression, result):
+        self.assertRaises(result, lambda: self.calc.evaluate(expression))
 
 if __name__ == '__main__':
     unittest.main()
